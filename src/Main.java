9726c42967dc61a2091b0867ff2141d3f8a3b55e
Rikza <maryamrikza@gmail.com>
2026-01-04 23:12:41 +0500

    initial GUI setup with main window

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("study planner");
+        frame.setSize(400,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
